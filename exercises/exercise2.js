d560d990491773daf1328862bed00b37bb8007d1
bethannrogers77@gmail.com <bethannrogers77@gmail.com>
2020-12-14 20:02:10 -0600

    pretty

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -6,7 +6,7 @@ module.exports.botScripts = [
   //
   // mychatbot> I'm tired
   // mychatbot responds "Wake up!"
-  
+
   {
     label: 'tired',
     prompt: 'I am tired',
@@ -18,50 +18,44 @@ module.exports.botScripts = [
     isListening: true,
   },
 
-
-
-  // Write an interaction where one user asks the bot 
+  // Write an interaction where one user asks the bot
   // "What's the area of a circle with radius X?" where X is any number
   // The chatbot will respond to the user with the answer.
-  // 
+  //
   // mychatbot> mychatbot What's the area of a circle with radius 5?
   // mychatbot responds to user "The area is 78.5398...."
   {
     label: 'An Example Script with Typed Variables',
     prompt: 'What is the area of a circle with a radius #{amount as number}',
     handler: function (params) {
-      const area = Math.PI * (params.amount * params.amount) ;
-      return (
-        'Area = ' +
-        Math.ceil(area)
-                
-      );
+      const area = Math.PI * (params.amount * params.amount);
+      return 'Area = ' + Math.ceil(area);
     },
     isReply: false,
     isCaseSensitive: false,
     isListening: true,
   },
 
-
-
-
-  // Write an interaction where one user asks the bot 
+  // Write an interaction where one user asks the bot
   // "Tell me a story with NAME1 and NAME2" where NAME1 and NAME2 is any string
   // The chatbot will respond with the answer.
-  // 
+  //
   // mychatbot> mychatbot Tell me a story with Jack and Jill
   // mychatbot responds to user "Jack and Jill went up the hill to fetch a pail of water."
   {
     label: 'Tell story',
     prompt: 'Tell me a story with #{name1} and #{name2}',
     handler: function (params) {
-      return  params.name1 + ' and ' +  params.name2 + ' went up the hill to fetch a pail of water.';
-           
+      return (
+        params.name1 +
+        ' and ' +
+        params.name2 +
+        ' went up the hill to fetch a pail of water.'
+      );
     },
 
     isReply: false,
     isCaseSensitive: false,
     isListening: true,
   },
-
-  ];
+];
--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -2,56 +2,50 @@
 module.exports.ACTIVATE_BOT = true;
 
 module.exports.botScripts = [
- 
-{
-  label: 'Valley Girl Math Star, seriously',
-  prompt: 'say hi to #{name}',
-  handler: function (params) {
-    return 'Like welcome to Valley Girl Math Star demo, ' + params.name + '!';
+  {
+    label: 'Valley Girl Math Star, seriously',
+    prompt: 'say hi to #{name}',
+    handler: function (params) {
+      return 'Like welcome to Valley Girl Math Star demo, ' + params.name + '!';
+    },
+    isReply: false,
+    isCaseSensitive: false,
+    isListening: true,
   },
-  isReply: false,
-  isCaseSensitive: false,
-  isListening: true,
-},
 
-{  label: 'celsius to farenheit',
-  prompt: 'Enter celsius, rad conversion to farenheit #{amount as number}',
-  handler: function (params) {
-    const temperature = Math.round( (params.amount * 9/5 ) + 32);    
-    return 'So very ' + temperature + '!';
+  {
+    label: 'celsius to farenheit',
+    prompt: 'Enter celsius, rad conversion to farenheit #{amount as number}',
+    handler: function (params) {
+      const temperature = Math.round((params.amount * 9) / 5 + 32);
+      return 'So very ' + temperature + '!';
+    },
+    isListening: true,
   },
-  isListening: true,
-},
 
-{
-  label: 'number squared',
-  prompt: 'Like enter number to be squared #{amount as number}',
-  handler: function (params) {
-    const squaredNum = (params.amount * params.amount) ;
-    return (
-      'AWESOME ' + squaredNum + '!'            
-    );
+  {
+    label: 'number squared',
+    prompt: 'Like enter number to be squared #{amount as number}',
+    handler: function (params) {
+      const squaredNum = params.amount * params.amount;
+      return 'AWESOME ' + squaredNum + '!';
+    },
+    isReply: false,
+    isCaseSensitive: false,
+    isListening: true,
   },
-  isReply: false,
-  isCaseSensitive: false,
-  isListening: true,
-},
-  // Write an interaction where one user asks the bot 
+  // Write an interaction where one user asks the bot
   // "What's the area of a circle with radius X?" where X is any number
   // The chatbot will respond to the user with the answer.
-  // 
+  //
   // mychatbot> mychatbot What's the area of a circle with radius 5?
   // mychatbot responds to user "The area is 78.5398...."
   {
     label: 'circle calc',
     prompt: 'Enter a radius to see the area of a circle #{amount as number}',
     handler: function (params) {
-      const area = Math.PI * (params.amount * params.amount) ;
-      return (
-        'Excellent, Area = ' +
-        Math.ceil(area)
-                
-      );
+      const area = Math.PI * (params.amount * params.amount);
+      return 'Excellent, Area = ' + Math.ceil(area);
     },
     isReply: false,
     isCaseSensitive: false,
@@ -61,10 +55,8 @@ module.exports.botScripts = [
     label: 'square root',
     prompt: 'As if, enter num and will give square root #{amount as number}',
     handler: function (params) {
-      const sqRoot = Math.sqrt(params.amount) ;
-      return (
-        'Rad ! it is ' +  sqRoot               
-      );
+      const sqRoot = Math.sqrt(params.amount);
+      return 'Rad ! it is ' + sqRoot;
     },
     isReply: false,
     isCaseSensitive: false,
@@ -75,10 +67,8 @@ module.exports.botScripts = [
     label: 'cosine calc',
     prompt: 'enter num of an angle to find the cosine #{amount as number}',
     handler: function (params) {
-      const cosineNum = Math.cos(params.amount) ;
-      return (
-        'Whatever! answer is ' + cosineNum + '!'             
-      );
+      const cosineNum = Math.cos(params.amount);
+      return 'Whatever! answer is ' + cosineNum + '!';
     },
     isReply: false,
     isCaseSensitive: false,
@@ -89,19 +79,14 @@ module.exports.botScripts = [
     prompt: 'Like was this math demo rad N or Y #{yesno}',
     handler: function (params) {
       if (params.yesno === 'Y') {
-      return 'TOTALLY RAD DUDE RETURN ANYTIME FOR MORE RAD MATH! '; 
-      }
-      else{
-      return 'WHATEVER DUDE, AS IF!';  
+        return 'TOTALLY RAD DUDE RETURN ANYTIME FOR MORE RAD MATH! ';
+      } else {
+        return 'WHATEVER DUDE, AS IF!';
       }
     },
-      isReply: false,
-      isCaseSensitive: false,
-      
-      isListening: true,
- },
+    isReply: false,
+    isCaseSensitive: false,
 
+    isListening: true,
+  },
 ];
-
-
-
